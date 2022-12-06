ae7f83b0873e11c3b34afc0713d0ed0c99770222
spathan <spathan@fortinet.com>
2022-12-06 12:12:06 +0530

    Addressed review changes

--- fortinet-fortirecon-bp/operations.py
+++ fortinet-fortirecon-bp/operations.py
@@ -1,21 +1,6 @@
-"""
-This file will be auto-generated on each "new operation action", so avoid editing in this file.
-"""
 from datetime import datetime
 import requests
-# import time
 from connectors.core.connector import get_logger, ConnectorError
-
-# from connectors.core.utils import update_connnector_config
-
-
-error_msg = {
-    401: 'Authentication failed due to invalid credentials',
-    429: 'Rate limit was exceeded',
-    403: 'Token is invalid or expired',
-    "ssl_error": 'SSL certificate validation failed',
-    'time_out': 'The request timed out while trying to connect to the remote server',
-}
 
 logger = get_logger("fortinet-fortirecon-bp")
 
@@ -26,17 +11,17 @@ class MakeRestApiCall:
         self.server_url = config.get('server_url', '').strip().strip('/')
         if not self.server_url.startswith('http') or not self.server_url.startswith('https'):
             self.server_url = 'https://' + self.server_url
+        self.org_id = config.get("org_id")
         self.authkey = config.get("api_key", '')
         self.verify_ssl = config.get("verify_ssl", True)
 
     def make_request(self, endpoint='', params=None, data=None, method='GET', headers=None, url=None, json_data=None):
         try:
             if url is None:
-                url = self.server_url + endpoint
+                url = self.server_url + endpoint.format(org_id=self.org_id)
 
             headers = {"Content-Type": "application/json",
                        "Authorization": self.authkey}
-            # headers["Authorizaion"] = self.authkey
             response = requests.request(method=method, url=url,
                                         headers=headers, data=data, json=json_data, params=params,
                                         verify=self.verify_ssl)
@@ -48,16 +33,27 @@ class MakeRestApiCall:
                     return response.text
             else:
                 logger.error("Error: {0}".format(response.json()))
-                raise ConnectorError('{0}'.format(error_msg.get(response.status_code, response.text)))
+                raise ConnectorError('{0}:{1}'.format(response.status_code, response.text))
         except requests.exceptions.SSLError as e:
             logger.exception('{0}'.format(e))
-            raise ConnectorError('{0}'.format(error_msg.get('ssl_error')))
+            raise ConnectorError('{0}'.format(e))
         except requests.exceptions.ConnectionError as e:
             logger.exception('{0}'.format(e))
-            raise ConnectorError('{0}'.format(error_msg.get('time_out')))
+            raise ConnectorError('{0}'.format(e))
         except Exception as e:
             logger.error('{0}'.format(e))
             raise ConnectorError('{0}'.format(e))
+
+    def organization_mapping(self, params):
+        endpoint = "/orgs"
+        response = self.make_request(endpoint=endpoint, method="GET")
+        org_mapping = response["hits"]
+        for org in org_mapping:
+            if org.get("org_name") == params.get("organization_id"):
+                org_id = org.get("org_id")
+
+        del params["organization_id"]
+        return org_id, params
 
 
 def _check_health(config: dict) -> bool:
@@ -71,112 +67,98 @@ def _check_health(config: dict) -> bool:
         raise Exception(e)
 
 
-def get_alert_details_by_id(config: dict, params: dict) -> dict:
-    org_id = params.pop("org_id")
-    alert_id = params.pop("id")
-    endpoint = f"/bp/{org_id}/alerts/{alert_id}"  # edit endpoint
-    method = "GET"  # GET/POST/PUT/DELETE
-
+def search_alerts(config: dict, params: dict) -> dict:
     MK = MakeRestApiCall(config=config)
+
+    endpoint = "/bp/{org_id}/alerts"
+    method = "GET"
+    if params.get("start_date"):
+        params["start_date"] = handle_date(params.get("start_date"))
+    if params.get("end_date"):
+        params["end_date"] = handle_date(params.get("end_date"))
+
+    response = MK.make_request(endpoint=endpoint, method=method, params=params)
+    return response
+
+
+def get_alert_details_by_id(config: dict, params: dict) -> dict:
+    MK = MakeRestApiCall(config=config)
+    alert_id = params.pop("id")
+    endpoint = "/bp/{org_id}/alerts/"+"{0}".format(alert_id)
+    method = "GET"
     response = MK.make_request(endpoint=endpoint, method=method, params=params)
     return response
 
 
 def get_phishing_campaigns(config: dict, params: dict) -> dict:
-    org_id = params.pop("org_id")
-    endpoint = f"/bp/{org_id}/phishing_campaigns"  # edit endpoint
-
-    method = "GET"  # GET/POST/PUT/DELETE
-    # write your code here, if needed.
-
     MK = MakeRestApiCall(config=config)
+    endpoint = "/bp/{org_id}/phishing_campaigns"
+
+    method = "GET"
     response = MK.make_request(endpoint=endpoint, method=method, params=params)
     return response
 
 
 def get_phishing_campaigns_by_id(config: dict, params: dict) -> dict:
-    endpoint = "/bp/{0}/phishing_campaigns/{1}".format(params.pop("org_id"), params.pop("id"))  # edit endpoint
-    method = "GET"  # GET/POST/PUT/DELETE
-    # write your code here, if needed.
-
     MK = MakeRestApiCall(config=config)
+    endpoint = "/bp/{org_id}/phishing_campaigns"+"/{0}".format(params.pop("id"))
+    method = "GET"
     response = MK.make_request(endpoint=endpoint, method=method, params=params)
     return response
 
 
 def get_rogue_app_by_id(config: dict, params: dict) -> dict:
-    endpoint = "/bp/{0}/rogue_apps/{1}".format(params.pop("org_id"), params.pop("id"))  # edit endpoint
-    method = "GET"  # GET/POST/PUT/DELETE
-    # write your code here, if needed.
-
     MK = MakeRestApiCall(config=config)
+    endpoint = "/bp/{org_id}/rogue_apps" + "/{0}".format(params.pop("id"))
+    method = "GET"
+
     response = MK.make_request(endpoint=endpoint, method=method, params=params)
     return response
 
 
 def get_rogue_apps(config: dict, params: dict) -> dict:
-    org_id = params.pop("org_id")
-    endpoint = f"/bp/{org_id}/rogue_apps"  # edit endpoint
-    method = "GET"  # GET/POST/PUT/DELETE
-    # write your code here, if needed.
     MK = MakeRestApiCall(config=config)
+    endpoint = "/bp/{org_id}/rogue_apps"
+    method = "GET"
     response = MK.make_request(endpoint=endpoint, method=method, params=params)
     return response
 
 
 def get_takedown_requests(config: dict, params: dict) -> dict:
-    org_id = params.pop("org_id")
-    endpoint = f"/bp/{org_id}/takedowns"  # edit endpoint
-    method = "GET"  # GET/POST/PUT/DELETE
-
     MK = MakeRestApiCall(config=config)
+    endpoint = "/bp/{org_id}/takedowns"
+    method = "GET"
+    if params.get("start_date"):
+        params["start_date"] = handle_date(params.get("start_date"))
+    if params.get("end_date"):
+        params["end_date"] = handle_date(params.get("end_date"))
     response = MK.make_request(endpoint=endpoint, method=method, params=params)
     return response
 
 
 def get_typo_domain_by_id(config: dict, params: dict) -> dict:
-    endpoint = "/bp/{0}/typo_domains/{1}".format(params.pop("org_id"), params.pop("id"))  # edit endpoint
-    method = "GET"  # GET/POST/PUT/DELETE
-    # write your code here, if needed.
-
     MK = MakeRestApiCall(config=config)
+    endpoint = "/bp/{org_id}/typo_domains"+"/{0}".format(params.pop("id"))
+    method = "GET"
     response = MK.make_request(endpoint=endpoint, method=method, params=params)
     return response
 
 
 def get_typo_domains(config: dict, params: dict) -> dict:
-    org_id = params.pop("org_id")
-    endpoint = f"/bp/{org_id}/typo_domains"  # edit endpoint
-    method = "GET"  # GET/POST/PUT/DELETE
-    # write your code here, if needed.
+    MK = MakeRestApiCall(config=config)
+    endpoint = "/bp/{org_id}/typo_domains"
+    method = "GET"
     if params.get("start_date"):
         params["start_date"] = handle_date(params.get("start_date"))
     if params.get("end_date"):
         params["end_date"] = handle_date(params.get("end_date"))
-
-    MK = MakeRestApiCall(config=config)
     response = MK.make_request(endpoint=endpoint, method=method, params=params)
     return response
 
 
 def handle_date(str_date):
-    return datetime.strptime(str_date, "%Y-%m-%dT%H:%M:%S.%fZ").date()
-
-
-def search_alerts(config: dict, params: dict) -> dict:
-    org_id = params.pop("org_id")
-    endpoint = f"/bp/{org_id}/alerts"  # edit endpoint
-    method = "GET"  # GET/POST/PUT/DELETE
-    # write your code here, if needed.
-    if params.get("start_date"):
-        params["start_date"] = handle_date(params.get("start_date"))
-    if params.get("end_date"):
-        params["end_date"] = handle_date(params.get("end_date"))
-
-    MK = MakeRestApiCall(config=config)
-    response = MK.make_request(endpoint=endpoint, method=method, params=params)
-    return response
-
+    return datetime.strptime(str_date, "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d")
+    # return datetime.strptime(str_date, "%Y-%m-%dT%H:%M:%S.%fZ").date()
 
 operations = {
     "search_alerts": search_alerts,
@@ -187,5 +169,5 @@ operations = {
     "get_takedown_requests": get_takedown_requests,
     "get_typo_domains": get_typo_domains,
     "get_rogue_app_by_id": get_rogue_app_by_id,
-    "get_typo_domain_by_id": get_typo_domain_by_id,
+    "get_typo_domain_by_id": get_typo_domain_by_id
 }
